ca2199941bef0e26a7b117f8ed9d807196f51cb6
sangwon2659 <60720982+sangwon2659@users.noreply.github.com>
2021-09-08 12:00:32 +0900

    Update aruco_data_collection.py

--- Sub PC/opencv_object_tracking/scripts/aruco_data_collection.py
+++ Sub PC/opencv_object_tracking/scripts/aruco_data_collection.py
@@ -3,23 +3,29 @@ import rospy
 import numpy as np
 from opencv_object_tracking.msg import Data_Single
 from opencv_object_tracking.msg import Data_Array
-sum = np.array([])
+sum_pitch = np.array([])
+sum_updown = np.array([])
 
 ###
-tolerance = 2.5
+tolerance_pitch = 1.8
+tolerance_updown = 2
 ###
 
 def callback(data):
-    global pub, sum
-    pitch = data.Data[1]
-    sum = np.append(sum,pitch)
-    if len(sum)>40:
-        sum = np.delete(sum,[0])
-        moving_avg = np.sum(sum)/40
-        if moving_avg-pitch>tolerance:
-            pub.publish(1)
+    global pub, sum_pitch, sum_updown
+    pitch = data.Data[0]
+    updown = data.Data[2]
+    sum_pitch = np.append(sum_pitch,pitch)
+    sum_updown = np.append(sum_updown, updown)
+    if len(sum_pitch)>40 and len(sum_updown)>40:
+        sum_pitch = np.delete(sum_pitch,[0])
+	sum_updown = np.delete(sum_updown,[0])
+	moving_avg_pitch = np.sum(sum_pitch)/40
+        moving_avg_updown = np.sum(sum_updown)/40
+        if moving_avg_pitch-pitch>tolerance_pitch or abs(moving_avg_updown-updown)>tolerance_updown:
+            pub.publish(1.0)
         else:
-            pub.publish(0)
+            pub.publish(0.0)
     else:
         rospy.loginfo("Collecting data for moving average computation")
 
