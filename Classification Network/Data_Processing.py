3fea8db644fe074bf59f53490a81c78f899b382c
sangwon2659 <60720982+sangwon2659@users.noreply.github.com>
2021-10-12 18:18:50 +0900

    Update Data_Processing.py

--- Classification Network/Data_Processing.py
+++ Classification Network/Data_Processing.py
@@ -5,13 +5,13 @@ import pandas as pd
 # Number of lookbacks for the covariance computation
 n_covariance = 10
 # Number of sample frequencies required
-n_frequency = 40
+n_frequency = 10
 # Scaling the covariance in order to match significance with the frequency data
 scale_covariance = 1000
 normalizer_constant = 1
 
 n_sample_frequency = n_frequency*2
-FSS = np.loadtxt("FSS_Data.csv", delimiter=",")
+FSS = np.loadtxt("Dataset.csv", delimiter=",")
 FSS_sum_array = np.array((0.0))
 FSS_previous = FSS[n_sample_frequency-1]
 Diff_Matrix = np.zeros((n_covariance, n_covariance))
@@ -21,6 +21,7 @@ Combined_Vector_Sum = np.zeros(n_sample_frequency/2+1)
 for i in range(np.shape(FSS)[0]):
     FSS_sum = FSS[i].sum()/normalizer_constant
     FSS_sum_array = np.append(FSS_sum_array, FSS_sum)
+
     if len(FSS_sum_array) > n_sample_frequency:
         FSS_sum_array = np.delete(FSS_sum_array, [0])
 
@@ -33,10 +34,11 @@ for i in range(np.shape(FSS)[0]):
         Diff_Matrix = np.delete(Diff_Matrix, n_covariance, axis=0)
         # Updating FSS_previous with the latest FSS data
         FSS_previous[:] = FSS[i][:]
+
         # Computing the covariance
         Cov_Matrix = np.dot(np.transpose(Diff_Matrix), Diff_Matrix)
         # Inserting the covariance value into the last position of Slip Vector array
-        Covariance = np.sum(Cov_Matrix) - np.trace(Cov_Matrix) * scale_covariance
+        Covariance = (np.sum(Cov_Matrix) - np.trace(Cov_Matrix)) * scale_covariance
 
         ### Frequency
         FFT = np.fft.fft(FSS_sum_array)
@@ -48,8 +50,7 @@ for i in range(np.shape(FSS)[0]):
         Combined_Vector[0:n_sample_frequency/2] = FFT
         Combined_Vector[n_sample_frequency/2] = Covariance
 
-        Combined_Vector_Sum = np.vstack((Combined_Vector, Combined_Vector_Sum))
+        Combined_Vector_Sum = np.vstack((Combined_Vector_Sum, Combined_Vector))
 
 df = pd.DataFrame(Combined_Vector_Sum)
-df.to_csv('Processed_Data.csv')
-
+df.to_csv('Processed_Data_10Hz.csv')
